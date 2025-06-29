cb889aad688839710008c45b1559a9eac5f9c799
Raunak Mukherjee <153613701+raunakmukherjee123@users.noreply.github.com>
2025-06-29 13:08:56 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,15 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const[n,setN]=useState(0);
+  const onAdd=()=>{
+    useState[n+1];
+  }
   return (
     <div>
         {/* Do not remove the main div */}
+        <p>Button clicked {n} times</p>
+        <button onClick={onAdd}>Click me</button>
     </div>
   )
 }
