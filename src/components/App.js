29b5aca97794b0b6c25f638daf33e8890c11a441
Raunak Mukherjee <153613701+raunakmukherjee123@users.noreply.github.com>
2025-06-29 13:13:04 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,13 +3,12 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
-  const[n,setN]=useState(0);
+  const [n,setN]=useState(0);
   const onAdd=()=>{
     setN(n+1);
   }
   return (
     <div>
-        {/* Do not remove the main div */}
         <p>Button clicked {n} times</p>
         <button onClick={onAdd}>Click me</button>
     </div>
