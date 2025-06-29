9d3b23e1a483bb5b3a6f81ff208346e81c46756d
Raunak Mukherjee <153613701+raunakmukherjee123@users.noreply.github.com>
2025-06-29 13:17:25 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,30 @@
+// import React from "react";
 
-import React from "react";
-import './../styles/App.css';
+// const App = () => {
+//   const [n,setN]=useState(0);
+//   const onAdd=()=>{
+//     setN(n+1);
+//   }
+//   return (
+//     <div>
+//         <p>Button clicked {n} times</p>
+//         <button onClick={onAdd}>Click me</button>
+//     </div>
+//   )
+// }
+//  export default App
 
-const App = () => {
-  const [n,setN]=useState(0);
-  const onAdd=()=>{
-    setN(n+1);
+import React,{useState} from "react";
+const App = ()=>{
+  const [count,setCount] = useState(0)
+  const onAdd = ()=>{
+    setCount(count+1)
   }
   return (
-    <div>
-        <p>Button clicked {n} times</p>
-        <button onClick={onAdd}>Click me</button>
+    <div >
+    <p>Button clicked {count} times</p>
+    <button onClick={onAdd}>Click me</button>
     </div>
   )
 }
-
 export default App
